24d91f5eae565c564114f58858761b8f1dbb73c5
Felix Stürmer <felix.stuermer@actano.de>
2016-05-04 12:05:39 +0200

    RX-6016: Move default http mock database content

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const cleverbridgeMockV1 = require('./cleverbridge/v1')
 const run = (env) => {
   const SERVER_ADDRESS = env.MOCK_SERVER_ADDRESS || '0.0.0.0'
   const SERVER_PORT = env.MOCK_SERVER_PORT || 8082
+  const CLEVERBRIDGE_V1_STATE_PATH =
+    env.MOCK_SERVER_CLEVERBRIDGE_V1_STATE_PATH || 'var/cleverbridge.json'
 
   const server = jsonServer.create()
   const defaultMiddlewares = jsonServer.defaults()
 
   server.use(defaultMiddlewares)
-  server.use('/cleverbridge/v1', cleverbridgeMockV1(env, 'cleverbridge.json'))
+  server.use(
+    '/cleverbridge/v1',
+    cleverbridgeMockV1(env, CLEVERBRIDGE_V1_STATE_PATH)
+  )
 
   server.listen(SERVER_PORT, SERVER_ADDRESS, () => {
     console.log(`Mock server is running on ${SERVER_ADDRESS}:${SERVER_PORT}...`)
