4fd7413f2dbcdb3d625d7a373539d38f243e224a
Felix Stürmer <felix.stuermer@actano.de>
2016-05-04 17:37:30 +0200

    RX-6016: Add payment information to mock subscriptions

--- cleverbridge/v1/signup.js
+++ cleverbridge/v1/signup.js
@@ -28,10 +28,28 @@ module.exports = (env, db) => {
   router.get('/perform-signup', (req, res) => {
     const subscriptionId = faker.random.uuid()
     const subscriptionRenewalDate = faker.date.future()
+    const creditCardExpirationDate = faker.date.future()
     const subscription = {
       customer: {
         your_customer_id: req.query.user_id,
-        status: 'active'
+        default_currency_iso_code: 'EUR',
+        default_contact: {
+          locale: 'en-US',
+          first_name: faker.name.firstName(),
+          last_name: faker.name.lastName(),
+          company: faker.company.companyName(),
+          street: faker.address.streetName(),
+          zip_code: faker.address.zipCode(),
+          city: faker.address.city(),
+          country_iso_code: 'US',
+          email: faker.internet.email()
+        },
+        payment_information: {
+          payment_method: 'visa',
+          card_last_four_digits: faker.random.number({min: 1000, max: 9999}),
+          card_expiration_date: `${creditCardExpirationDate.getMonth()}/${creditCardExpirationDate.getFullYear()}`
+        },
+        remote_ip: faker.internet.ip()
       },
       id: subscriptionId, // for mock retrieval
       next_billing_at: subscriptionRenewalDate,
