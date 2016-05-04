520a508aa25e5849d39fa3536c247b6955251f62
Felix Stürmer <felix.stuermer@actano.de>
2016-05-04 18:00:15 +0200

    RX-6016: Consistently use plural for resources

--- cleverbridge/v1/index.js
+++ cleverbridge/v1/index.js
@@ -4,7 +4,7 @@ const jsonServer = require('json-server')
 const authentication = require('./authentication')
 const invoices = require('./invoices')
 const signup = require('./signup')
-const subscription = require('./subscriptions')
+const subscriptions = require('./subscriptions')
 
 module.exports = (env, dbSource) => {
   const jsonServerRouter = jsonServer.router(dbSource)
@@ -12,7 +12,7 @@ module.exports = (env, dbSource) => {
 
   router.use(authentication(env, jsonServerRouter.db))
   router.use(signup(env, jsonServerRouter.db))
-  router.use(subscription(env, jsonServerRouter.db))
+  router.use(subscriptions(env, jsonServerRouter.db))
   router.use(invoices(env, jsonServerRouter.db))
   router.use(jsonServerRouter)
 
