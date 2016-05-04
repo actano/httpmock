d38ac8065d2f35f43add331ed5c15842f4c6a98a
Felix Stürmer <felix.stuermer@actano.de>
2016-05-04 14:13:32 +0200

    RX-6016: Add route to force subscription expiration
    A subscription expiration can now be forced by making a `POST` request
    to the mock http server's route
    `/cleverbridge/v1/subscription/:subscription_id/expire`.

--- cleverbridge/v1/subscriptions.js
+++ cleverbridge/v1/subscriptions.js
@@ -46,5 +46,45 @@ module.exports = (env, db) => {
     }
   })
 
+  router.post('/subscriptions/:subscription_id/expire', (req, res) => {
+    const subscriptionId = req.params.subscription_id
+    const subscription = db('subscriptions').updateById(subscriptionId, {
+      canceled_at: new Date().toISOString(),
+      status: 'canceled'
+    })
+
+    if (!subscription) {
+      res.status(404).send('Subscription not found.')
+    } else {
+      const notification = {
+        notification_id: `notification_${subscriptionId}_cancelation_performed`,
+        client_id: '',
+        event: {
+          type: 'subscription.cancelation_performed',
+          id: `event_${subscriptionId}_cancelation_performed`,
+          occurred_at: new Date().toISOString()
+        },
+        resource: {
+          type: 'subscription',
+          id: subscriptionId,
+          current_state: subscription
+        }
+      }
+
+      utils.createNotificationRequest(env, notification)
+        .end((err) => {
+          if (err) {
+            res
+              .status(500)
+              .send(`An error occured while sending the notification: ${err}`)
+
+            return
+          }
+
+          res.json(subscription)
+        })
+    }
+  })
+
   return router
 }
